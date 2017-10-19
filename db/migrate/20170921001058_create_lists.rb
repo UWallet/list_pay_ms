e0f1950fb523024b6a6d21fd612ce779b12a092f
Andres Felipe Sanchez Lemus <andfsanchezlem@unal.edu.co>
2017-10-18 19:26:08 -0500

    boolean state

--- db/migrate/20170921001058_create_lists.rb
+++ db/migrate/20170921001058_create_lists.rb
@@ -3,10 +3,10 @@ class CreateLists < ActiveRecord::Migration[5.1]
     create_table :lists do |t|
       t.integer :user_id
       t.text :description
-      t.date :date_pay
+      t.string :date_pay
       t.integer :cost
       t.integer :target_account
-      t.text :state_pay
+      t.boolean :state
 
       t.timestamps
     end
